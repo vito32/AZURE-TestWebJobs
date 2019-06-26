ffa2a379690f2db3cbd12497c5d4b37f7401a7f7
vito32 <32vito@gmail.com>
2019-06-26 12:02:43 +0200

    Add files via upload

---
+++ Models/Auction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobsAuctions.Models
+{
+    public class Auction
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime OpeningTime { get; set; }
+
+        public DateTime ClosingTime { get; set; }
+
+        public ICollection<AuctionItem> Items { get; set; }
+    }
+}
---
+++ Models/AuctionDbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JobsAuctions.Models
+{
+    public class AuctionDbInitializer : DropCreateDatabaseIfModelChanges<AuctionDbContext>
+    {
+        protected override void Seed(AuctionDbContext context)
+        {
+            base.Seed(context);
+
+            if(context.Auctions.Count() != 1)
+            {
+                var auction = new Auction
+                {
+                    Title = "Nostalgic Items",
+                    Description = "",
+                    OpeningTime = DateTime.Now.Subtract(TimeSpan.FromDays(1)),
+                    ClosingTime = DateTime.Now.AddDays(1),
+                    Items = new List<AuctionItem>
+                    {
+                        new AuctionItem
+                        {
+                            Name = "Clash of the Titans Lunchbox",
+                            Description = "Stop motion magic for lunch",
+                            StartingAmount = 25,
+                            Donor = "Matt Milner"
+                        }
+                    }
+                };
+
+                context.Auctions.Add(auction);
+                context.SaveChanges();
+            }
+        }
+    }
+}
